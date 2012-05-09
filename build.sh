6ce5069b6a41aa31a785f407a5b190b9e297c5eb
Mark Kennard <komcomputers@gmail.com>
2012-05-09 23:55:41 +1200

    Add build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ -z $1 ]; then
+	if [ -z $KBUILD_BUILD_VERSION ]; then
+		export KBUILD_BUILD_VERSION="Test-`date '+%Y%m%d-%H%m'`"
+	fi
+	echo "Kernel will be labelled ($KBUILD_BUILD_VERSION)"
+else
+	echo "Setting kernel name to ($1)"
+	export KBUILD_BUILD_VERSION=$1
+fi
+
+echo "Compiling the kernel"
+rm arch/arm/boot/zImage
+make -j2 CROSS_COMPILE=/usr/arm-toolchain/bin/arm-eabi- ARCH=arm
+
+if test -f arch/arm/boot/zImage; then
+	echo "Tarballing the kernel"
+	cp arch/arm/boot/zImage ./
+	tar cf $KBUILD_BUILD_VERSION-zImage.tar zImage
+	rm zImage
+else
+	echo "Will not tarball as make didn't produce zImage"
+fi
+
+echo "Done"
